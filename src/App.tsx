b5c606f1fe6e4d075febdc4a0d0767e6f1e641b1
thejustedd <eduard.kirsha@gmail.com>
2022-08-04 20:26:32 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  const hello = 'Hello';
+  const world = 'World';
+
+  return (
+    <>
+      <h2>Hello</h2>
+    </>
+  );
+};
+
+export { App };
